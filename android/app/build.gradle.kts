217326d967e90276869739494c7288d36dea8beb
memberstalk-lab <memberstalk@gmail.com>
2026-03-08 14:55:50 +0300

    Updated .gitignore

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.kiddorewards.app"
-    compileSdk = 34
+    namespace = "com.example.kiddo_rewards"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,9 +20,12 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.kiddorewards.app"
-        minSdk = 23
-        targetSdk = 34
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        applicationId = "com.example.kiddo_rewards"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
