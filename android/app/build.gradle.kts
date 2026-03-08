0d45ea68a296093695a1f05e00aa0b4b9ba1864c
memberstalk-lab <memberstalk@gmail.com>
2026-03-08 13:07:03 +0300

    Added ANDROID_SDK_34_UPGRADE.md

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.kiddo_rewards"
-    compileSdk = flutter.compileSdkVersion
+    namespace = "com.kiddorewards.app"
+    compileSdk = 34
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -20,12 +20,9 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.kiddo_rewards"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        applicationId = "com.kiddorewards.app"
+        minSdk = 23
+        targetSdk = 34
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
